f9cbee33e56459fb733174a65722a77adb4d8acc
Valyan <161570222+Valentine-doc@users.noreply.github.com>
2024-09-26 13:40:02 +0300

    Update Client.cs
    Изменено чисто по синтаксису

--- DevelopmentNetworkApplicationLess3/Client.cs
+++ DevelopmentNetworkApplicationLess3/Client.cs
@@ -56,7 +56,6 @@ namespace DevelopmentNetworkApplicationLess3
                         Console.WriteLine(userServer.ToString());
                     }
                 }
-
             }
         }
     }
